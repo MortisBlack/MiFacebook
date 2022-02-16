7654487e0dfb20297fd5f9d3cc0220d4f2899a69
MortisBlack <jesus.urrego.iuc@gmail.com>
2022-02-16 00:00:09 -0700

    Initial commit

---
+++ app/src/main/java/urrego/jesus/mifacebook/MainActivity.kt
@@ -0,0 +1,12 @@
+package urrego.jesus.mifacebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio_face)
+        //setContentView(R.layout.registro_face)
+    }
+}
